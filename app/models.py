e3fbd55c4eda6863fabfc20a5b817c4362c8ec0b
caritasem <caritasem@foxmail.com>
2024-01-04 23:43:06 +0800

    010405

--- app/models.py
+++ app/models.py
@@ -10,9 +10,9 @@ class Note(Base):
     __tablename__ = 'notes'
     id = Column(Integer, primary_key=True, index=True,
                 autoincrement=True)
-    title = Column(String, nullable=False)
-    content = Column(String, nullable=False)
-    category = Column(String, nullable=True)
+    title = Column(String(32), nullable=False)
+    content = Column(String(256), nullable=False)
+    category = Column(String(32), nullable=True)
     published = Column(Boolean, nullable=False, default=True)
     createdAt = Column(TIMESTAMP(timezone=True),
                        nullable=False, server_default=func.now())
@@ -22,14 +22,14 @@ class Note(Base):
 
 class Jobs(Base):
     __tablename__ = 'jobs'
-    id = Column(String, primary_key=True, nullable=False, index=True)
-    img_name = Column(String, nullable=False)
-    img_path = Column(String, nullable=False)
-    img_rgb = Column(String, nullable=True)
-    img_prompt = Column(String, nullable=False)
+    id = Column(String(64), primary_key=True, nullable=False, index=True)
+    img_name = Column(String(64), nullable=False)
+    img_path = Column(String(200), nullable=False)
+    img_rgb = Column(String(200), nullable=True)
+    img_prompt = Column(String(200), nullable=False)
     max_steps = Column(Integer, nullable=False, default=1000)
     step = Column(Integer, nullable=False, default=0, index=True)
-    d3_path = Column(String, nullable=True)
+    d3_path = Column(String(200), nullable=True)
     createdAt = Column(TIMESTAMP(timezone=True),
                        nullable=False, server_default=func.now())
     updatedAt = Column(TIMESTAMP(timezone=True),
